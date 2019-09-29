1d66719df9d30f90b7972623ab0c21ea269dea8e
VivekV95 <vivekv210@gmail.com>
2019-09-28 23:51:45 -0500

    Create BaseActivity for shared ProgressBar

---
+++ app/src/main/java/com/vivekvishwanath/restmvvmdemo/activity/RecipeListActivity.kt
@@ -0,0 +1,13 @@
+package com.vivekvishwanath.restmvvmdemo.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vivekvishwanath.restmvvmdemo.R
+
+class RecipeListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_list)
+    }
+}
